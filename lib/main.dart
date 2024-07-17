ec22870deb9fe7668b1402b67c182e8cfd3cde83
Lenin Bose <leninbose1596@gmail.com>
2024-07-18 02:48:21 +0530

    Initial Commit: Expense Tracker App built using Flutter.

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:expense_tracker_flutter/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
